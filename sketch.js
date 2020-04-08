5f915e02ab9455a85bee0b921076b91adb7071a1
fruitfont <63322232+fruitfont@users.noreply.github.com>
2020-04-07 19:34:53 -0500

    Graphics quality fixed (?)
    "density() not a function" fixed

--- sketch.js
+++ sketch.js
@@ -149,7 +149,7 @@ function mousePressed() {
   if(mouseY>140&&mouseY<190) {
     if(mouseIsPressed) {
       let density = map(mouseX, 0, width, 0.01, 1); 
-      density = ceil(100*density());
+      density = ceil(100*density);
       pixelDensity(map(mouseX, 0, width, 0.01, 1));
     }
   }
