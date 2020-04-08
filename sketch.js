2497a0237bcb1382162d495377dcf2a1416344f1
fruitfont <63322232+fruitfont@users.noreply.github.com>
2020-04-07 19:00:14 -0500

    fixed (hopefully) source

--- sketch.js
+++ sketch.js
@@ -48,23 +48,23 @@ let myFont;
 
 
 function preload() {
-  myFont = loadFont('assets/Sweet.otf');
-  txtr.headR = loadImage('assets/headR.png');
-  txtr.legR = loadImage('assets/legR.png');
-  txtr.headL = loadImage('assets/headL.png');
-  txtr.legL = loadImage('assets/legL.png');
-  txtr.armL = loadImage('assets/armL.png');
-  txtr.armR = loadImage('assets/armR.png');
-  txtr.handL = loadImage('assets/handL.png');
-  txtr.handR = loadImage('assets/handR.png');
-  txtr.axL = loadImage('assets/axL.png');
-  txtr.axR = loadImage('assets/axR.png');
-  txtr.tree0 = loadImage('assets/tree0.png');
-  txtr.tree1 = loadImage('assets/tree1.png');
-  txtr.tip = loadImage('assets/tip.png');
-  txtr.meat = loadImage('assets/meat.png');
-  txtr.shop = loadImage('assets/shop.png');
-  txtr.gear = loadImage('assets/gear.png');
+  myFont = loadFont('/assets/Sweet.otf');
+  txtr.headR = loadImage('/assets/headR.png');
+  txtr.legR = loadImage('/assets/legR.png');
+  txtr.headL = loadImage('/assets/headL.png');
+  txtr.legL = loadImage('/assets/legL.png');
+  txtr.armL = loadImage('/assets/armL.png');
+  txtr.armR = loadImage('/assets/armR.png');
+  txtr.handL = loadImage('/assets/handL.png');
+  txtr.handR = loadImage('/assets/handR.png');
+  txtr.axL = loadImage('/assets/axL.png');
+  txtr.axR = loadImage('/assets/axR.png');
+  txtr.tree0 = loadImage('/assets/tree0.png');
+  txtr.tree1 = loadImage('/assets/tree1.png');
+  txtr.tip = loadImage('/assets/tip.png');
+  txtr.meat = loadImage('/assets/meat.png');
+  txtr.shop = loadImage('/assets/shop.png');
+  txtr.gear = loadImage('/assets/gear.png');
 }
 
 function setup() {
